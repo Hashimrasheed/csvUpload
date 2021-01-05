32918181a81061de1a0b8bc9e509ef9ebcb55030
Hashim <hashimrasheed1999@gmail.com>
2021-01-05 22:08:34 +0530

    fixed bug in redirecting

--- app/http/controllers/userController.js
+++ app/http/controllers/userController.js
@@ -8,11 +8,12 @@ const amqp = require('amqplib/callback_api')
 function userController() {
     return {
         async home(req, res) {
+            let ids = await db.get().collection(collection.CSVUPLOAD).aggregate([]).toArray();
+
             try {
-                let ids = await db.get().collection(collection.CSVUPLOAD).aggregate([]).toArray();
                 res.render('user/home', { ids })
             } catch (e) {
-                ids = ''
+                ids = null
                 res.render('user/home', { ids })
             }
 
@@ -22,58 +23,63 @@ function userController() {
             res.render('user/addfile', { objId })
         },
         async addfile(req, res) {
-            let csvData = req.files.csvfile.data.toString();
-            let csvfile = req.files.csvfile
-            let id = req.params.id
-            await csvtojson().fromString(csvData).then(json => {
-                csvfile.mv(`public/csvfiles/${id}.csv`, async (err) => {
-                    if (!err) {
-                        amqp.connect('amqps://ffvqpvmk:njT7VxX-BGBRk4ZOMohI7JSfYW2V1Z4H@owl.rmq.cloudamqp.com/ffvqpvmk', (connError, connection) => {
-                            if (connError) {
-                                throw connError;
-                            }
-                            connection.createChannel((channelError, channel) => {
-                                if (channelError) {
-                                    throw channelError;
+            try {
+                let csvData = req.files.csvfile.data.toString();
+                let csvfile = req.files.csvfile
+                let id = req.params.id
+                await csvtojson().fromString(csvData).then(() => {
+                    csvfile.mv(`public/csvfiles/${id}.csv`, async (err) => {
+                        if (!err) {
+                            await amqp.connect('amqps://ffvqpvmk:njT7VxX-BGBRk4ZOMohI7JSfYW2V1Z4H@owl.rmq.cloudamqp.com/ffvqpvmk', async (connError, connection) => {
+                                if (connError) {
+                                    throw connError;
                                 }
-                                const QUEUE = 'CSVTEST'
-                                channel.assertQueue(QUEUE);
-                                let obj = { id: id, path: `public/csvfiles/${id}.csv` }
-                                channel.sendToQueue(QUEUE, Buffer.from(JSON.stringify(obj)));
-                                console.log(`message send ${QUEUE}`);
+                                await connection.createChannel(async (channelError, channel) => {
+                                    if (channelError) {
+                                        throw channelError;
+                                    }
+                                    const QUEUE = 'CSVTEST'
+                                    await channel.assertQueue(QUEUE);
+                                    let obj = { id: id, path: `public/csvfiles/${id}.csv` }
+                                    await channel.sendToQueue(QUEUE, Buffer.from(JSON.stringify(obj)));
+                                    console.log(`message send ${QUEUE}`);
+                                    // await channel.close()
 
-                            })
-
-                        })
-                        amqp.connect('amqps://ffvqpvmk:njT7VxX-BGBRk4ZOMohI7JSfYW2V1Z4H@owl.rmq.cloudamqp.com/ffvqpvmk', (connError, connection) => {
-                            if (connError) {
-                                throw connError;
-                            }
-                            connection.createChannel((channelError, channel) => {
-                                if (channelError) {
-                                    throw channelError;
-                                }
-                                const QUEUE = 'CSVTEST'
-                                channel.assertQueue(QUEUE);
-                                channel.consume(QUEUE, (msg) => {
-                                    console.log(`message is ${msg.content}`);
-                                    let json = JSON.parse(msg.content.toString())
-                                    csvtojson().fromFile(json.path).then(async (data) => {
-                                        await db.get().collection(collection.CSVUPLOAD).insertOne({ _id: ObjectID(json.id), details: data })
-                                        res.redirect(`/`)
-                                    })
-
-                                }, {
-                                    noAck: true
                                 })
-                            })
+                                // await connection.close()
 
-                        })
-                    } else {
-                        console.log(err);
-                    }
+                            })
+                            await amqp.connect('amqps://ffvqpvmk:njT7VxX-BGBRk4ZOMohI7JSfYW2V1Z4H@owl.rmq.cloudamqp.com/ffvqpvmk', async (connError, connection) => {
+                                if (connError) {
+                                    throw connError;
+                                }
+                                await connection.createChannel(async (channelError, channel) => {
+                                    if (channelError) {
+                                        throw channelError;
+                                    }
+                                    const QUEUE = 'CSVTEST'
+                                    await channel.assertQueue(QUEUE);
+                                    await channel.consume(QUEUE, (msg) => {
+                                        console.log(`message is ${msg.content}`);
+                                        let json = JSON.parse(msg.content.toString())
+                                        csvtojson().fromFile(json.path).then((data) => {
+                                            db.get().collection(collection.CSVUPLOAD).insertOne({ _id: ObjectID(json.id), details: data })
+                                        })
+                                    }, {
+                                        noAck: true
+                                    })
+                                })
+                                res.redirect(`/`)
+                            })
+                        } else {
+                            res.redirect(`/generate`)
+                        }
+                    })
                 })
-            })
+            } catch (e) {
+                throw e
+            }
+
         },
         async singleFile(req, res) {
             try {
